3d1b2a5372b5b6ce84f78dc4d8a6994657d3023c
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-11-13 17:13:45 +0100

    8 tasks completed

---
+++ builtin.c
@@ -0,0 +1,134 @@
+/*
+ * File: builtin.c
+ * Auth: Gbenga Elegbede
+ *       Ebenezer Sam-Oladapo
+ */
+
+#include "shell.h"
+
+
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_cd(char **args, char **front);
+
+/**
+ * get_builtin - Matches a command with a corresponding
+ *               shellby builtin function.
+ * @command: The command to match.
+ *
+ * Return: A function pointer to the corresponding builtin.
+ */
+
+int (*get_builtin(char *command))(char **args, char **front)
+{
+	builtin_t funcs[] = {
+		{ "exit", shellby_exit },
+		{ "env", shellby_env },
+		{ "setenv", shellby_setenv },
+		{ "unsetenv", shellby_unsetenv },
+		{ "cd", shellby_cd },
+	/*	{ "alias", shelly_alias },*/
+		{ NULL, NULL }
+	};
+	int i;
+	for (i = 0; funcs[i].name; i++)
+	{
+		if (_strncmp(funcs[i].name, command, _strlen(funcs[i].name)) == 0)
+			break;
+	}
+	return (funcs[i].f);
+}
+/**
+ * shellby_exit - Causes normal process termination
+ *                for the shellby shell.
+ * @args: An array of arguments containing the exit value.
+ *
+ * Return: If there are no arguments - -3.
+ *         If the given exit value is invalid - 2.
+ *         O/w - exits with the given status value.
+ *
+ * Description: Upon returning -3, the program exits back in the main function.
+ */
+int shellby_exit(char **args, char **front)
+{
+	int i = 0, sign = 1;
+	unsigned int num = 0;
+	if (args[0])
+	{
+		if (args[0][i] == '-')
+			sign = -1;
+		for (; args[0][i]; i++)
+		{
+			if (args[0][i] == '-')
+				sign *= -1;
+			if (args[0][i] >= '0' && args[0][i] <= '9')
+				num = (num * 10) + (args[0][i] - '0');
+			else
+				return (2);
+		}
+	}
+	else
+	{
+		return (-3);
+	}
+	args -= 1;
+	free_args(args, front);
+	free_env();
+	exit(num * sign);
+}
+/**
+ * shellby_cd - Changes the current directory of the shellby process.
+ * @args: An array of arguments.
+ *
+ * Return: If the given string is not a directory - 2.
+ *         If an error occurs - -1.
+ *         Otherwise - 0.
+ */
+int shellby_cd(char **args, char **front)
+{
+	char **dir_info;
+	char *oldpwd = NULL, *pwd = NULL;
+	struct stat dir;
+	oldpwd = getcwd(oldpwd, 0);
+	if (!oldpwd)
+		return (-1);
+	if (args[0])
+	{
+		if (*(args[0]) == '-')
+			chdir(*(_getenv("OLDPWD")) + 7);
+		else
+		{
+			if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode)
+					&& ((dir.st_mode & S_IXUSR) != 0))
+				chdir(args[0]);
+			else
+			{
+				free(oldpwd);
+				return (2);
+			}
+		}
+	}
+	else
+		chdir(*(_getenv("HOME")) + 5);
+	pwd = getcwd(pwd, 0);
+	if (!pwd)
+		return (-1);
+	dir_info = malloc(sizeof(char *) * 2);
+	if (!dir_info)
+		return (-1);
+
+	dir_info[0] = "OLDPWD";
+	dir_info[1] = oldpwd;
+	if (shellby_setenv(dir_info, front) == -1)
+		return (-1);
+
+	dir_info[0] = "PWD";
+	dir_info[1] = pwd;
+	if (shellby_setenv(dir_info, front) == -1)
+		return (-1);
+
+	free(oldpwd);
+	free(pwd);
+	free(dir_info);
+	return (0);
+}
